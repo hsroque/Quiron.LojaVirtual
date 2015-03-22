cf35d06a3e2def049840fdc83a28b8d576466dd7
hsroque <hs.roque@gmail.com>
2015-03-22 18:51:52 -0300

    commit 6 aula criando context e o repositorio

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext context = new EfDbContext();
+
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return context.Produtos; }
+        }
+
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio repositorio;
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            repositorio = new ProdutosRepositorio();
+
+            var produtos = repositorio.Produtos.Take(10);
+
+            return View(produtos);
+        }
+    }
+}
