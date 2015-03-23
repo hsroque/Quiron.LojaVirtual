0361e349d930fd3eeaeffed80795799c6526bdf5
hsroque <hs.roque@gmail.com>
2015-03-23 20:21:36 -0300

    Commit Aula 8 criando classe paginação

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -7,16 +7,20 @@ using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
-    public class ProdutoController : Controller
+    public class VitrineController : Controller
     {
         private ProdutosRepositorio repositorio;
 
+        public int ProdutosPorPagina = 3;
         // GET: Produto
-        public ActionResult Index()
+        public ActionResult ListaProdutos(int pagina = 1)
         {
             repositorio = new ProdutosRepositorio();
 
-            var produtos = repositorio.Produtos.Take(10);
+            var produtos = repositorio.Produtos
+                            .OrderBy(p => p.Descricao)
+                            .Skip((pagina - 1) * ProdutosPorPagina)
+                            .Take(ProdutosPorPagina);
 
             return View(produtos);
         }
