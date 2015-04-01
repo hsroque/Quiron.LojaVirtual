1c2350c04c79ab62fe1deb8451ba286e2c8f3483
hsroque <hs.roque@gmail.com>
2015-03-31 22:08:32 -0300

    Aula 18 e 19 Criando view e controller carrinho

---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Web.Models;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        // GET: Carrinho
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produto != null)
+                ObterCarrinho().AdicionarItem(produto, 1);
+
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+
+            if (produtoId != null)
+                ObterCarrinho().RemoverItem(produto);
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho)Session["Carrinho"];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                ReturnUrl = returnUrl
+            });
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
