de7ffd35bc94cf90ac32adc85f97a8f7262bb8ad
hsroque <hs.roque@gmail.com>
2015-04-07 20:51:16 -0300

    Aula 28 - Iniciando a criação da area administrativa

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        // GET: Administrativo/Produto
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            var produtos = _repositorio.Produtos;
+
+            return View(produtos);
+        }
+    }
+}
